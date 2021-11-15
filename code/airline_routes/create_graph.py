f718959460414b402893793061128a6b69997825
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2021-11-15 19:26:19 +0100

    Updates
    - Redefined the TODOs
    - Upgraded the visualization of the graph

--- code/airline_routes/create_graph.py
+++ code/airline_routes/create_graph.py
@@ -18,25 +18,31 @@ def main():
     # print(wd_path)
 
     # range(100) because I need to test the method fast (can't wait to plot more then 5k airports).
-    for i in tqdm(range(100)):
+    for i in tqdm(range(6)):
         # Take an airport from the "airport.dat" dataset
         source_airport = airports_set[i]
         # Extrapolate its city (the source city)
         city = source_airport[2]
         # Extrapolate its ID
         source_airport_ID = source_airport[8]
-        for route in routes:
-            # From all the routes I need to find the destination airports from the initial airport
-            if route[3] == source_airport_ID:
-                # I have found one!
-                destination_airport_ID = route[5]
-                # Now I need to find the city of this "destination airport" (destination city)
-                for dest_airport in airports_set:
-                    if dest_airport[8] == destination_airport_ID:
-                        dest_city = dest_airport[2]
-                        # Let's create an edge between the "source city" and the "destination city"
-                        G.add_edge(city, dest_city)
-    nx.drawing.draw(G)
+        print(city, source_airport_ID)
+        if city != "":
+            for route in routes:
+                # From all the routes I need to find the destination airports from the initial airport
+                if route[3] == source_airport_ID:
+                    # I have found one!
+                    destination_airport_ID = route[5]
+                    # Now I need to find the city of this "destination airport" (destination city)
+                    for dest_airport in airports_set:
+                        if dest_airport[8] == destination_airport_ID:
+                            if dest_airport[2] != "":
+                                dest_city = dest_airport[2]
+                                # Let's create an edge between the "source city" and the "destination city"
+                                G.add_edge(city, dest_city)
+    nx.drawing.draw(G, node_size=10)
+    # TODO: show the graph on a planisphere
+    # TODO: Give the name only to nodes which have more than x neighbours
+    # TODO: Weight edges
     plt.show()
 
 

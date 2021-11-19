fa50b0f9702477736b0ea5b442fd0ff640766076
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2021-11-19 12:09:50 +0100

    Updated Utility class

--- code/airline_routes/Utility.py
+++ code/airline_routes/Utility.py
@@ -4,10 +4,24 @@ import pathlib
 
 wd_path = os.path.abspath(os.path.join(os.path.join(pathlib.Path().resolve(), '..'), '..'))
 
+# If the files airports.dat and/or routes.dat are updated then we should update also routes_complete.csv
+# -> update = True
+update = False
 
-# TODO: Function which creates routes_complete.csv from airport.dat and routes.dat
+
+def create_routes_complete():
+    # TODO: Function which (re)creates routes_complete.csv from airport.dat and routes.dat
+    pass
+
 
 def read_routes():
+    """
+    TODO
+    :return: matrix with all routes_complete.csv entries
+    """
+    # Before reading routes_complete.csv we check if it should be update
+    if update:
+        create_routes_complete()
     rows = []
     file = open(wd_path + "/data/airline_routes_data/routes_complete.csv", encoding="utf-8")
     csv_reader = csv.reader(file)

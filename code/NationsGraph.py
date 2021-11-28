6694ac8bd6cb995278d60ba7d341b63c8ec2d9b2
Alberto Ursino <alberto.ursino@studenti.unipd.it>
2021-11-28 13:29:17 +0100

    Created the nations graph

---
+++ code/NationsGraph.py
@@ -0,0 +1,50 @@
+import networkx as nx
+
+
+def extrapolate_country_edges(graph):
+    """
+    Extrapolates city edges from a graph and create a dictionary of triplets in the form (source_country, dest_country, weight)
+    :return: instance of a dictionary
+    """
+    edges = {}
+    weight = 1
+    for instance in graph.edges():
+        source_node = graph.nodes[instance[0]]
+        dest_node = graph.nodes[instance[1]]
+        edge = (source_node["country"], dest_node["country"])
+        if not (edge in edges):
+            edges[edge] = [weight]
+        else:
+            edges[edge][0] += 1
+    return edges
+
+
+def build_nations_graph(edge_dict):
+    """
+    Takes an edge dictionary in the form {('source_node', 'dest_node'): [x], ...} where x is the weight of the edge and
+    builds a graph with it.
+    :param edge_dict: instance of a dictionary
+    :return: instance of a graph
+    """
+    G = nx.Graph()
+    for instance in edge_dict:
+        G.add_edge(instance[0], instance[1], weight=edge_dict[instance][0])
+    return G
+
+
+def main():
+    # Builds nations graph for sister cities
+    edge_dict = extrapolate_country_edges(
+        nx.readwrite.read_gexf('../data/sister_cities_data/reduced_sister_cities.gexf'))
+    G = build_nations_graph(edge_dict)
+    nx.write_gexf(G, '../data/sister_cities_data/reduced_nations_sister_cities.gexf')
+
+    # Builds nations graph for airline routes
+    edge_dict = extrapolate_country_edges(
+        nx.readwrite.read_gexf('../data/airline_routes_data/reduced_routes.gexf'))
+    G = build_nations_graph(edge_dict)
+    nx.write_gexf(G, '../data/airline_routes_data/reduced_nations_routes.gexf')
+
+
+if __name__ == "__main__":
+    main()

e0371b400b55f1034ddb3f84586e45ba5560f4a8
WittyDude <mari8.marco@gmail.com>
2021-12-16 19:37:16 +0100

    codice più modulare

---
+++ code/graph_builders/AirlineRoutes.py
@@ -0,0 +1,58 @@
+import networkx as nx
+from tqdm import tqdm
+import csv
+from .. import Utility
+from ..Utility import ar_dir_path
+
+
+def build_graph():
+    """
+    # This method builds the airline routes graph
+    :return: an instance of the graph
+    """
+    # All the nodes will be stored in "cities", all the edges in "edges" and in positions the coordinates
+    # associated to each city.
+    edges = []
+    cities = {}
+
+    file = open(ar_dir_path + "routes_complete.csv", encoding="utf-8")
+    routes = csv.reader(file)
+    next(routes)  # skip header
+    for entry in tqdm(routes, desc="Creating airline routes graph"):
+        source_city = entry[4]
+        dest_city = entry[9]
+        if source_city != "#N/D" and dest_city != "#N/D":
+            if source_city not in cities:
+                cities[source_city] = {'lat': float(entry[5]),
+                                       'lon': float(entry[6]),
+                                       'country': entry[12]}
+            if dest_city not in cities:
+                cities[dest_city] = {'lat': float(entry[10]),
+                                     'lon': float(entry[11]),
+                                     'country': entry[13]}
+
+            edges.append((source_city, dest_city))
+
+    G = nx.Graph()
+
+    for node, attr in cities.items():
+        G.add_node(node, **attr)
+
+    for edge in tqdm(edges, desc="Weighting edges"):
+        # Each edge will be weighted based on how many times the edge shows up.
+        if G.has_edge(edge[0], edge[1]):
+            G[edge[0]][edge[1]]['weight'] += 1
+        else:
+            G.add_edge(edge[0], edge[1], weight=1)
+
+    nx.write_gexf(G, ar_dir_path + "routes.gexf")
+    return G
+
+
+def main():
+    G = build_graph()
+    Utility.save_plot(G, ar_dir_path + 'complete_routes_plot.png')
+
+
+if __name__ == "__main__":
+    main()
---
+++ code/graph_builders/SisterCities.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+import networkx as nx
+from tqdm import tqdm
+from .. import Utility
+from ..Utility import ss_dir_path
+
+cities = {}
+sisters = set()
+prefixes = ['city', 'sister']
+
+
+def add_city(record, cid, prefix):
+    """
+    # TODO
+    :param record:
+    :param cid:
+    :param prefix:
+    :return:
+    """
+    if cid in cities:
+        cities[cid]['degree'] += 1
+    else:
+        lon, lat = Utility.coord(record[f'{prefix}_coordinate_location']['value'])
+        cities[cid] = {
+            # How many times we encounter this city
+            'degree': 1,
+            # This is the the name of the city or its sister depending on prefix
+            'label': record[f'{prefix}Label']['value'],
+            'lon': lon,
+            'lat': lat,
+            'pop': int(record[f'{prefix}_population']['value']),
+            'country': record[f'{prefix}_countryLabel']['value']
+        }
+
+
+def parse_response(resp):
+    """
+    # TODO
+    :param resp:
+    :return:
+    """
+    for record in tqdm(resp['results']['bindings'], desc="Parsing"):
+        # in general we get something like http://www.wikidata.org/entity/Q84 (London)
+        # we retain only the code Q84
+        city_id = record['city']['value'].split('/')[-1]
+        sister_id = record['sister']['value'].split('/')[-1]
+
+        relation_id = tuple(sorted([city_id, sister_id]))
+        sisters.add(relation_id)
+
+        add_city(record, city_id, prefix='city')
+        add_city(record, sister_id, prefix='sister')
+
+
+def build_graph():
+    """
+    Builds the sister cities graph inserting nodes (cities) and edges (sister bond)
+    :return: an instance of the graph
+    """
+    query = Path(ss_dir_path + 'sister_cities_query.sparql').absolute().read_text()
+    resp = Utility.request(query)
+    parse_response(resp)
+    G = nx.Graph()
+    for cid, attr in tqdm(cities.items(), desc="Adding nodes"):
+        # attr is a dictionary with information about the city
+        G.add_node(cid, **attr)
+    for sister in tqdm(sisters, desc="Adding edges"):
+        G.add_edge(sister[0], sister[1])
+    nx.write_gexf(G, ss_dir_path + 'sister_cities.gexf')
+    return G
+
+
+def main():
+    G = build_graph()
+    Utility.save_plot(G, ss_dir_path + 'complete_sister_cities_plot.png')
+
+
+if __name__ == "__main__":
+    main()
